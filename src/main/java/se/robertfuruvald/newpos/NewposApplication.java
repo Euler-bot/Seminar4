72a54a70bcb0a7a339562474dbedeb8421ac44f8
Robert Furuvald <robertfuruvald@gmail.com>
2022-04-30 23:03:28 +0200

    starting new project

---
+++ src/main/java/se/robertfuruvald/newpos/NewposApplication.java
@@ -0,0 +1,13 @@
+package se.robertfuruvald.newpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewposApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/robertfuruvald/newpos/NewposApplicationTests.java
@@ -0,0 +1,13 @@
+package se.robertfuruvald.newpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewposApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
